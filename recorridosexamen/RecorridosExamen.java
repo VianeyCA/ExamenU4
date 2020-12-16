59cad6a778f0809cfb050a3fde3e1b044f038288
Vianey Ambrosio <71795759+VianeyCA@users.noreply.github.com>
2020-12-15 19:11:58 -0600

    Update RecorridosExamen.java

--- recorridosexamen/RecorridosExamen.java
+++ recorridosexamen/RecorridosExamen.java
@@ -4,59 +4,41 @@ package recorridosexamen;
 import java.util.Scanner;
 
 public class RecorridosExamen {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-        int opc=0;
-         int elemento;
-         String nombre;
-         ArbolBinario arbol=new ArbolBinario();
-         ArbolBinario arbolE= new ArbolBinario();
-         
-         Scanner leer= new Scanner(System.in);
+            public static void main(String[] args) {
+         // TODO code application logic here
+         Scanner leer = new Scanner(System.in);
+         Arbol arbol = new Arbol();
+         int dato;
+         System.out.println("----------------Inserción de Datos----------------");
          for (int i = 0; i < 10; i++) {
-             System.out.println("Primer Arbol");
-             System.out.print("Ingresa el numero del nodo: ");
-             elemento=leer.nextInt();
-             System.out.print("Ingresa el nombre del nodo: ");
-             nombre=leer.next();
-             arbol.agregarNodo(elemento, nombre);
-             System.out.println(""); 
-        }
-         for (int i = 0; i < 10; i++) {
-             System.out.println("Segundo Arbol");
-             System.out.print("Ingresa el numero del nodo: ");
-             elemento=leer.nextInt();
-             System.out.print("Ingresa el nombre del nodo: ");
-             nombre=leer.next();
-             arbolE.agregarNodo(elemento, nombre);
-             System.out.println(""); 
-        }
-         
-         System.out.println("--------------------Primer Arbol--------------------");
-         System.out.println("Recorrido en In-Orden");
-         if(!arbol.estaVacio()){
-             arbol.inOrden(arbol.raiz);
-             }else{
-                   System.out.println("El árbol esta vacio");
-                  }
-         System.out.println("Recorrido en  Pre-Orden");
-         arbol.PreOrden(arbol.raiz);
-         
-         System.out.println("Recorrido en Pos-Orden");
-         arbol.PostOrden(arbol.raiz);
-         System.out.println("--------------------Segundo Arbol--------------------");
-         System.out.println("Recorrido en In-Orden");
-         if(!arbolE.estaVacio()){
-             arbolE.inOrden(arbolE.raiz);
-             }else{
-                   System.out.println("El árbol esta vacio");
-                  }
-         System.out.println("Recorrido en  Pre-Orden");
-         arbolE.PreOrden(arbolE.raiz);
-         
-         System.out.println("Recorrido en Pos-Orden");
-         arbolE.PostOrden(arbolE.raiz);
+             System.out.print("Ingresa un nodo: ");
+             dato = leer.nextInt();
+             arbol.agregar(dato);
+         }
+         System.out.println("");
+         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+         System.out.println("                    Impresión                     ");
+         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+         System.out.println("");
+         System.out.println("+------------------Arbol Normal--------------------");
+         System.out.println("Pre-Orden");
+         arbol.PreOrden(arbol.getRn());
+         System.out.println("");
+         System.out.println("In-Orden");
+         arbol.PostOrden(arbol.getRn());
+         System.out.println("");
+         System.out.println("Post-Orden");
+         arbol.InOrden(arbol.getRn());
+         System.out.println("");
+         System.out.println("");
+         System.out.println("--------------------Arbol Espejo-------------------");
+         System.out.println("Pre-Orden");
+         arbol.PreOrden(arbol.getRe());
+         System.out.println("");
+         System.out.println("In-Orden");
+         arbol.InOrden(arbol.getRe());
+         System.out.println("");
+         System.out.println("Post-Orden");
+         arbol.PostOrden(arbol.getRe());
+         System.out.println("");
     }
-    
-}
